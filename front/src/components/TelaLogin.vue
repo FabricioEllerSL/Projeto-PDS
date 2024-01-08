b4cadd081adc7cbb1826032c80dfaf0d9dfe6cde
FabricioEllerSL <fabricioellersl@gmail.com>
2024-01-08 18:00:14 -0300

    nav bar login

--- front/src/components/TelaLogin.vue
+++ front/src/components/TelaLogin.vue
@@ -3,7 +3,7 @@
         name: 'TelaLogin',
         data(){
             return{
-                
+                dataAtual: new Date().toLocaleDateString(),
             }
         }
     }
@@ -11,6 +11,25 @@
 
 <template>
     <div>
-        <p class="bg-red ma-9">Caralhos</p>
+        <nav class="nav-bar d-flex justify-space-between align-center px-16">
+            <v-btn to="/" class="rounded-xl px-8 login-button">Início</v-btn>
+            <v-text>PsychoSystem &copy; 2023</v-text>
+            <v-text>{{ dataAtual }}</v-text>
+        </nav>
     </div>
 </template>
+
+<style scoped>
+.nav-bar{
+    background-color:  #fff;
+    margin: 0;
+    height: 70px;
+    opacity: 1;
+}
+
+.login-button{
+    background-color: #edd7dd !important;
+    color: #555;
+    font-size: 20px;
+}    
+</style>
